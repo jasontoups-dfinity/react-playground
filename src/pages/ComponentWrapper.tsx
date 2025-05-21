9b398e792a673e363f02101b48b6f801094b916d
jasontoups-dfinity <jason.toups@dfinity.org>
2025-05-21 11:02:56 -0700

    adds tailwind post-css processor

--- src/pages/ComponentWrapper.tsx
+++ src/pages/ComponentWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ComponentWrapper: React.FC = () => {
   return (
-    <div className="container mx-auto p-6">
+    <main className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Component Wrapper</h1>
       <p className="mb-4">
         This page demonstrates a component wrapper pattern that allows for flexible component
@@ -12,7 +12,7 @@ const ComponentWrapper: React.FC = () => {
         <h2 className="text-xl font-semibold mb-4">Example Component</h2>
         <p>Content will be added here as we develop this pattern.</p>
       </div>
-    </div>
+    </main>
   );
 };
 
