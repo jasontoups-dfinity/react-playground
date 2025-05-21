3836260b38495372c249b79b2dc5c1bc12c8fa19
jasontoups-dfinity <jason.toups@dfinity.org>
2025-05-21 12:51:14 -0700

    added shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -14,4 +15,9 @@ const htmlPlugin = () => {
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), htmlPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
